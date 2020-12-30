c9142fd920a0acbd2cd0e6988cc288c83c845262
alinefo <aline.oliveira@unicid.edu.br>
2020-12-30 18:43:50 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -2,8 +2,43 @@ package com.nelioalves.cursomc.domain;
 
 import java.util.Date;
 
-public class PagamentoComBoleto extends Pagamento {
+import javax.persistence.Entity;
 
-		private Date dataVencimento;
-		private Date dataPagamento;
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+
+	private Date dataVencimento;
+	private Date dataPagamento;
+
+	public PagamentoComBoleto() {
+
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento,
+			Date dataPagamaneto) {
+		super(id, estado, pedido);
+		this.dataPagamento = dataPagamaneto;
+		this.dataVencimento = dataVencimento;
+
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
 }
--- src/main/java/com/nelioalves/cursomc/domain/enums/EstadoPagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/enums/EstadoPagamento.java
@@ -10,30 +10,34 @@ public enum EstadoPagamento {
 	private String descricao;
 	
 	private EstadoPagamento(int cod, String descricao) {
-		this.cod=cod;
-		this.descricao=descricao;
+		this.cod = cod;
+		this.descricao = descricao;
 	}
-	
+
 	public int getCod() {
 		return cod;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
 	
-//	public static EstadoPagamento toEnum (Integer cod) {
-//		if (cod ==null) {
-//			return null;
-//		}
-//		
-//		for (EstadoPagamento x : EstadoPagamento.values()) {
-//			if(cod.equals(x.getCod())) {
-//				return x;
-//			}
-//			
-//		}
-//		throw new IllegalArgumentException("Id inválido: " = cod);
-//	}
+	public static EstadoPagamento toEnum (Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for (EstadoPagamento x : EstadoPagamento.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+
+		
+	
+	
+	
 
 }
